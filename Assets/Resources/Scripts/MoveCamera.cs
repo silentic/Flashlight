91f1c7dab81584d8dfadb2f4c32ea4487053d05c
Pongsatorn Santiwatanakul <kirk.kirby@hotmail.com>
2015-07-03 15:19:08 +0700

    [Game] - create flash light and light systems - camera move along player

---
+++ Assets/Resources/Scripts/MoveCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+    private Transform player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerpos = player.position;
+        playerpos.z = transform.position.z;
+        transform.position = playerpos;
+	}
+}
