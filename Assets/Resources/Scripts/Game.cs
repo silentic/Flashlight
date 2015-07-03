36ce56d6bb5dd7221eee4479264c9eba912df42b
Arpple <rbo_sqat@hotmail.com>
2015-07-03 11:43:05 +0700

    add playercontrol (movement only , no turning) add movement controlpad

---
+++ Assets/Resources/Scripts/Game.cs
@@ -0,0 +1,19 @@
+﻿#define KEYBOARD
+
+using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour 
+{
+	public static GameObject player;
+
+	void Awake () 
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Resources/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿//#define KEYBOARD
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour 
+{
+
+
+	public float speed;
+
+	Vector3 direction;
+	Rigidbody2D playerRigidbody;
+
+	
+	void Awake()
+	{
+		playerRigidbody = GetComponent<Rigidbody2D>();
+		direction = Vector3.zero;
+	}
+
+	void Update () 
+	{
+#if KEYBOARD
+		direction.Set(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"),0);
+		direction.z = 0;
+		move (direction);
+#endif
+	}
+
+	public void move (Vector3 direction)
+	{
+		playerRigidbody.MovePosition(transform.position + direction * speed * Time.deltaTime);
+	}
+}
