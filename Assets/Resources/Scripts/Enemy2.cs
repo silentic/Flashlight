3bbab36a20e7377c67c4f879c913562501c4604f
Arpple <rbo_sqat@hotmail.com>
2015-07-08 10:50:13 +0700

    - enemy visible fixed (use Enter/Exit light event) - light radius fix (use scale)

---
+++ Assets/Resources/Scripts/Enemy2.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy2 : FOVScript_VLS
+{
+	
+}
--- Assets/Resources/Scripts/EnemyScript.cs
+++ Assets/Resources/Scripts/EnemyScript.cs
@@ -6,51 +6,90 @@ public class EnemyScript : MonoBehaviour
 	SpriteRenderer renderer;
 	Collider2D collider;
 	Transform player;
+	int visible;
 	// Use this for initialization
 	void Start () 
 	{
 		renderer = GetComponent<SpriteRenderer>();
 		collider = GetComponent<Collider2D>();
 		player = Game.player.transform;
+		visible = 0;
+
+		Light2D.RegisterEventListener(LightEventListenerType.OnEnter, OnLightEnter);
+		Light2D.RegisterEventListener(LightEventListenerType.OnExit, OnLightExit);
 	}
 	
 	// Update is called once per frame
+#if test
 	void Update () 
 	{
-		if(renderer.IsVisibleFrom(Camera.main))
+		//out of camera
+		if(!renderer.IsVisibleFrom(Camera.main))
 		{
-			Vector2 playerPos =	(Vector2)player.position;
-			Vector2 direction = (Vector2)transform.position - playerPos;
-			float range = direction.magnitude;
-
-			//check object block
-			RaycastHit2D hit = Physics2D.Raycast(playerPos,direction);
-			if(hit.collider != collider)
-			{
-				renderer.enabled = false;
-				return;
-			}
-	
-			//check ouf of maximum sight range
-			if(range > 6)
-			{
-				renderer.enabled = false;
-				return;
-			}
-
-			Vector2 playerLookRotation = (Vector2)(player.rotation * Vector3.up);
-			float angle = Vector2.Angle(playerLookRotation,direction);
-
-			//check facing
-			if(angle > 25 && range > 2.6f)
-			{
-				renderer.enabled = false;
-				return;
-			}
-			renderer.enabled = true;
+			renderer.enabled = false;
 			return;
 		}
-		renderer.enabled = false;
+
+		Vector2 playerPos =	(Vector2)player.position;
+		Vector2 direction = (Vector2)transform.position - playerPos;
+		float range = direction.magnitude;
+
+		//check object block
+		RaycastHit2D hit = Physics2D.Raycast(playerPos,direction);
+		if(hit.collider != collider)
+		{
+			renderer.enabled = false;
+			return;
+		}
+
+		//check ouf of maximum sight range
+		if(range > 6)
+		{
+			renderer.enabled = false;
+			return;
+		}
+
+		Vector2 playerLookRotation = (Vector2)(player.rotation * Vector3.up);
+		float angle = Vector2.Angle(playerLookRotation,direction);
+
+		//check facing
+		if(angle > 25 && range > 2.6f)
+		{
+			renderer.enabled = false;
+			return;
+		}
+
+		renderer.enabled = true;
+		return;
+	}
+#endif
+	void OnLightEnter(Light2D _light, GameObject _go)
+	{
+		if (_go.GetInstanceID() == gameObject.GetInstanceID())
+		{
+			if(visible <= 0)
+			{
+				Debug.Log("Object Entered Light");
+
+				renderer.enabled = true;
+			}
+			visible += 1;
+		}
+	}
+
+	void OnLightExit(Light2D _light, GameObject _go)
+	{
+		if (_go.GetInstanceID() == gameObject.GetInstanceID())
+		{
+			visible -= 1;
+			if(visible <= 0)
+			{
+				Debug.Log("Object Exited Light");
+
+				renderer.enabled = false;
+			}
+
+		}
 	}
 }
 
