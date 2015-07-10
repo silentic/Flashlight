d7d96bf2216c30846635ddbd77b8417e43e5c53e
Arpple <rbo_sqat@hotmail.com>
2015-07-10 11:47:34 +0700

    add : movement AI > random direction when find wall in front

--- Assets/Resources/Scripts/Enemy.cs
+++ Assets/Resources/Scripts/Enemy.cs
@@ -6,19 +6,28 @@ public class Enemy : MonoBehaviour
 	public float speed;
 	public float maxHp;
 	protected float hp;
+	Vector3 direction;
 
-	SpriteRenderer enemyRenderer;
+	protected SpriteRenderer enemyRenderer;
 	Rigidbody2D enemyRigidbody;
 	int visible;
+	bool moving;
+	
 	// Use this for initialization
 	protected virtual void Start () 
 	{
+		hp = maxHp;
+
 		enemyRenderer = GetComponent<SpriteRenderer>();
+		enemyRigidbody = GetComponent<Rigidbody2D>();
 		visible = 0;
+		moving = false;
 
 		Light2D.RegisterEventListener(LightEventListenerType.OnStay, OnLightStay);
 		Light2D.RegisterEventListener(LightEventListenerType.OnEnter, OnLightEnter);
 		Light2D.RegisterEventListener(LightEventListenerType.OnExit, OnLightExit);
+
+		move (randomPath());
 	}
 
 	protected virtual void OnDestroy()
@@ -91,13 +100,51 @@ public class Enemy : MonoBehaviour
 
 	public virtual void move (Vector3 direction)
 	{
-		enemyRigidbody.MovePosition(transform.position + direction * speed * Time.deltaTime);
-
+		RaycastHit2D hit = Physics2D.Raycast(transform.position , direction);
+		if(hit != null)
+		{
+			Debug.Log("TEST");
+			if(hit.distance < 2)
+				moving = false;
+			else
+			{	
+				moving = true;
+				this.direction = direction;
+				enemyRigidbody.MovePosition(transform.position + direction * speed * Time.deltaTime);
+				//move (direction);
+			}
+		}
 	}
 	
 	public virtual void turn (Vector3 direction)
 	{
 		transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+	}
+
+	void FixedUpdate()
+	{
+		if(moving)
+			move (direction);
+		else
+			move (randomPath());
+	}
+
+	Vector3 randomPath()
+	{
+		int r = Random.Range(0,4);
+		Vector3 direction;
+		switch(r)
+		{
+			case 0:
+				return Vector2.up;
+			case 1:
+				return Vector2.down;
+			case 2:
+				return Vector2.left;			
+			case 3:
+				return Vector2.right;
+		}
+		return Vector3.zero;
 	}
 }
 
