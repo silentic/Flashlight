8098aefd0198c8fce82f357967f64b10092445a8
Pongsatorn Santiwatanakul <kirk.kirby@hotmail.com>
2015-07-10 14:34:56 +0700

    create minimap incomplete

---
+++ Assets/Resources/Scripts/Minimap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimap : MonoBehaviour {
+
+    bool visible;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("m"))
+        {
+            visible = !visible;
+            gameObject.GetComponent<Camera>().enabled = visible;
+        }
+            
+	}
+}
---
+++ Assets/Resources/Scripts/ScreamScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreamScript : MonoBehaviour {
+
+    SpriteRenderer enemyRenderer;
+    int visible;
+    // Use this for initialization
+    void Start()
+    {
+        enemyRenderer = GetComponent<SpriteRenderer>();
+        visible = 0;
+
+        //Light2D.RegisterEventListener(LightEventListenerType.OnStay, OnStay);
+        Light2D.RegisterEventListener(LightEventListenerType.OnEnter, OnLightEnter);
+        Light2D.RegisterEventListener(LightEventListenerType.OnExit, OnLightExit);
+    }
+
+    void OnLightEnter(Light2D _light, GameObject _go)
+    {
+        if (_go.GetInstanceID() == gameObject.GetInstanceID())
+        {
+            if (visible <= 0)
+            {
+                Debug.Log("Object Entered Light");
+
+                enemyRenderer.enabled = true;
+            }
+            visible += 1;
+        }
+    }
+
+    void OnLightExit(Light2D _light, GameObject _go)
+    {
+        if (_go.GetInstanceID() == gameObject.GetInstanceID())
+        {
+            visible -= 1;
+            if (visible <= 0)
+            {
+                Debug.Log("Scream: Object Exited Light");
+
+                enemyRenderer.enabled = false;
+
+                Destroy(gameObject);
+            }
+
+        }
+    }
+
+    void OnDestroy()
+    {
+        /* (!) Make sure you unregister your events on destroy. If you do not
+         * you might get strange errors (!) */
+
+        //Light2D.UnregisterEventListener(LightEventListenerType.OnStay, OnLightStay);
+        Light2D.UnregisterEventListener(LightEventListenerType.OnEnter, OnLightEnter);
+        Light2D.UnregisterEventListener(LightEventListenerType.OnExit, OnLightExit);
+    }
+}
+
